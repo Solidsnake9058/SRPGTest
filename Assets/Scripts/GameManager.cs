a33ddb0777a269fe99c3095023beb08101ed31db
Solidsnake9058 <30305049+Solidsnake9058@users.noreply.github.com>
2017-07-20 10:45:25 +0800

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager inatance;
+
+    public GameObject tilePrefab;
+    public GameObject userPlayer;
+
+    public int mapHeight = 11;
+    public int mapWeight = 15;
+
+    List<List<Tile>> map = new List<List<Tile>>();
+    List<Player> players = new List<Player>();
+    int currentPlayerIndex = 0;
+
+    void Awake()
+    {
+        inatance = this;
+    }
+
+    private void Start()
+    {
+        genetareMap();
+        genetarePlayers();
+
+    }
+
+    void Update()
+    {
+        players[currentPlayerIndex].TurnUpdate();
+    }
+
+    public void nextTurn()
+    {
+        if (currentPlayerIndex + 1 < players.Count)
+        {
+            currentPlayerIndex++;
+        }
+        else
+        {
+            currentPlayerIndex = 0;
+        }
+    }
+
+    public void moveCurrentPlayer(Tile destTile)
+    {
+        players[currentPlayerIndex].moveDestination = destTile.transform.position + 1.5f * Vector3.up;
+    }
+
+    void genetareMap()
+    {
+        map = new List<List<Tile>>();
+        for (int i = 0; i < mapWeight; i++)
+        {
+            List<Tile> row = new List<Tile>();
+            for (int j = 0; j < mapHeight; j++)
+            {
+                Tile tile = ((GameObject)Instantiate(tilePrefab, new Vector3(i - Mathf.Floor(mapWeight / 2), 0, -j + Mathf.Floor(mapHeight / 2)), Quaternion.Euler(new Vector3()))).GetComponent<Tile>();
+                tile.gridPostion = new Vector2(i, j);
+                row.Add(tile);
+            }
+            map.Add(row);
+        }
+    }
+
+    void genetarePlayers()
+    {
+        UserPlayer player;
+
+        player = ((GameObject)Instantiate(userPlayer, new Vector3(0 - Mathf.Floor(mapWeight / 2), 1.5f, -0 + Mathf.Floor(mapHeight / 2)), Quaternion.Euler(new Vector3()))).GetComponent<UserPlayer>();
+
+        players.Add(player);
+
+        player = ((GameObject)Instantiate(userPlayer, new Vector3((mapWeight - 1) - Mathf.Floor(mapWeight / 2), 1.5f, -(mapHeight - 1) + Mathf.Floor(mapHeight / 2)), Quaternion.Euler(new Vector3()))).GetComponent<UserPlayer>();
+
+        players.Add(player);
+
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour
+{
+
+    public Vector2 gridPostion = Vector2.zero;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseEnter()
+    {
+        transform.GetComponent<Renderer>().material.color = Color.blue;
+
+        Debug.Log("Pos (" + gridPostion.x + "," + gridPostion.y + ")");
+    }
+
+    private void OnMouseExit()
+    {
+        transform.GetComponent<Renderer>().material.color = Color.white;
+
+    }
+
+    private void OnMouseDown()
+    {
+        GameManager.inatance.moveCurrentPlayer(this);
+    }
+}
