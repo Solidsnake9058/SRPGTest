7259d666626d5e8decf9b8c4e1879061e835d722
Solidsnake9058 <30305049+Solidsnake9058@users.noreply.github.com>
2017-09-12 02:32:40 +0800

    Update object creator Weapon,Race,Character(not finish)

--- Assets/Scripts/ObjectCreator/ObjectCreatorManager.cs
+++ Assets/Scripts/ObjectCreator/ObjectCreatorManager.cs
@@ -49,7 +49,7 @@ public class ObjectCreatorManager : MonoBehaviour
 
     //Race group
     private int raceId = -1;
-    List<int> raceWeapons;
+    private List<int> raceWeapons;
     [Header("Race group")]
     public InputField raceName;
     public Toggle raceCanFly;
@@ -84,6 +84,7 @@ public class ObjectCreatorManager : MonoBehaviour
     private List<CharacterType> races;
     private List<CharacterTemplate> characters;
     private List<CharacterLevelTemplate> characterLevels;
+
     private Dictionary<int, string> dicItemType;
     private Dictionary<int, string> dicRace;
     private Dictionary<int, string> dicWeapon;
@@ -105,6 +106,7 @@ public class ObjectCreatorManager : MonoBehaviour
         group.blocksRaycasts = true;
     }
 
+    //Click create type
     public void ControlGroup(Button btn)
     {
         DisableAllGroup();
@@ -148,8 +150,19 @@ public class ObjectCreatorManager : MonoBehaviour
                 }
                 break;
             case CreateType.weapon:
+                if (AddWeapon())
+                {
+                    ResetWeapon();
+                    LoadWeaponObjects();
+                    SetWeapon();
+                }
                 break;
             case CreateType.race:
+                if (AddRace())
+                {
+                    ResetRace();
+                    LoadRaceObjects();
+                }
                 break;
             case CreateType.character:
                 break;
@@ -165,15 +178,18 @@ public class ObjectCreatorManager : MonoBehaviour
                 break;
             case CreateType.item:
                 RemoveItem();
-                ReloadObjects();
                 break;
             case CreateType.weapon:
+                RemoveWeapon();
+                SetWeapon();
                 break;
             case CreateType.race:
+                RemoveRace();
                 break;
             case CreateType.character:
                 break;
         }
+        ReloadObjects();
     }
 
     public void UpdateObject()
@@ -184,29 +200,19 @@ public class ObjectCreatorManager : MonoBehaviour
                 break;
             case CreateType.item:
                 UpdateItem();
-                ReloadObjects();
                 break;
             case CreateType.weapon:
+                UpdateWeapon();
+                SetWeapon();
                 break;
             case CreateType.race:
+                UpdateRace();
+                SetRace();
                 break;
             case CreateType.character:
                 break;
         }
-    }
-
-    private void LoadItemObjects()
-    {
-        ClearObjectList();
-        items.Sort((x, y) => { return x.id.CompareTo(y.id); });
-
-        for (int i = 0; i < items.Count; i++)
-        {
-            GameObject newObject = (GameObject)Instantiate(objectSelectPrefab, Vector3.zero, Quaternion.Euler(new Vector3()));
-            newObject.GetComponent<Button>().name = items[i].id.ToString();
-            newObject.GetComponent<Button>().GetComponentInChildren<Text>().text = items[i].name;
-            newObject.transform.SetParent(objectList);
-        }
+        ReloadObjects();
     }
 
     private void DisableAllGroup()
@@ -224,6 +230,11 @@ public class ObjectCreatorManager : MonoBehaviour
         races = new List<CharacterType>();
         characters = new List<CharacterTemplate>();
         characterLevels = new List<CharacterLevelTemplate>();
+        raceWeapons = new List<int>();
+
+        dicItemType = new Dictionary<int, string>();
+        dicRace = new Dictionary<int, string>();
+        dicWeapon = new Dictionary<int, string>();
     }
 
     private void ReloadObjects()
@@ -237,14 +248,14 @@ public class ObjectCreatorManager : MonoBehaviour
                 LoadItemObjects();
                 break;
             case CreateType.weapon:
+                ResetWeapon();
                 EnableGroup(weaponGroup);
-
-                ClearObjectList();
+                LoadWeaponObjects();
                 break;
             case CreateType.race:
+                ResetRace();
                 EnableGroup(raceGroup);
-
-                ClearObjectList();
+                LoadRaceObjects();
                 break;
             case CreateType.character:
                 EnableGroup(charGroup);
@@ -262,8 +273,10 @@ public class ObjectCreatorManager : MonoBehaviour
                 LoadItem(id);
                 break;
             case CreateType.weapon:
+                LoadWeapon(id);
                 break;
             case CreateType.race:
+                LoadRace(id);
                 break;
             case CreateType.character:
                 break;
@@ -371,7 +384,7 @@ public class ObjectCreatorManager : MonoBehaviour
         Item tempItem = new Item();
         for (int i = 0; i < items.Count; i++)
         {
-            if (items[i].id == itemId)
+            if (items[i].id == tempId)
             {
                 tempItem = items[i];
                 items.RemoveAt(i);
@@ -381,6 +394,20 @@ public class ObjectCreatorManager : MonoBehaviour
         if (!AddItem(tempId))
         {
             items.Add(tempItem);
+        }
+    }
+
+    private void LoadItemObjects()
+    {
+        ClearObjectList();
+        items.Sort((x, y) => { return x.id.CompareTo(y.id); });
+
+        for (int i = 0; i < items.Count; i++)
+        {
+            GameObject newObject = (GameObject)Instantiate(objectSelectPrefab, Vector3.zero, Quaternion.Euler(new Vector3()));
+            newObject.GetComponent<Button>().name = items[i].id.ToString();
+            newObject.GetComponent<Button>().GetComponentInChildren<Text>().text = items[i].name;
+            newObject.transform.SetParent(objectList);
         }
     }
 
@@ -399,9 +426,9 @@ public class ObjectCreatorManager : MonoBehaviour
         weaponCanSell.isOn = false;
     }
 
-    private bool AddWeapon()
+    private bool AddWeapon(int newId = -1)
     {
-        if (string.IsNullOrEmpty(itemName.text))
+        if (string.IsNullOrEmpty(weaponName.text))
         {
             return false;
         }
@@ -416,7 +443,7 @@ public class ObjectCreatorManager : MonoBehaviour
             bool sell = weaponCanSell.isOn;
             bool atkTwice = weaponCanTwice.isOn;
 
-            int id = weapons.Count > 0 ? weapons.Select(x => x.id).Max() + 1 : 0;
+            int id = newId == -1 ? (weapons.Count > 0 ? weapons.Select(x => x.id).Max() + 1 : 0) : newId;
             if (weapons.Where(x => x.name == name).Count() > 0)
             {
                 Debug.LogError("Weapons exist");
@@ -463,8 +490,185 @@ public class ObjectCreatorManager : MonoBehaviour
         weaponId = -1;
     }
 
+    private void UpdateWeapon()
+    {
+        int tempId = weaponId;
+        Weapon tempWeapon = new Weapon();
+        for (int i = 0; i < weapons.Count; i++)
+        {
+            if (weapons[i].id == tempId)
+            {
+                tempWeapon = weapons[i];
+                weapons.RemoveAt(i);
+                break;
+            }
+        }
+        if (!AddWeapon(tempId))
+        {
+            weapons.Add(tempWeapon);
+        }
+    }
+
+    private void LoadWeaponObjects()
+    {
+        ClearObjectList();
+        weapons.Sort((x, y) => { return x.id.CompareTo(y.id); });
+
+        for (int i = 0; i < weapons.Count; i++)
+        {
+            GameObject newObject = (GameObject)Instantiate(objectSelectPrefab, Vector3.zero, Quaternion.Euler(new Vector3()));
+            newObject.GetComponent<Button>().name = weapons[i].id.ToString();
+            newObject.GetComponent<Button>().GetComponentInChildren<Text>().text = weapons[i].name;
+            newObject.transform.SetParent(objectList);
+        }
+    }
     #endregion
 
+    #region Race Control
+
+    private void ResetRace()
+    {
+        raceId = -1;
+        raceWeapons = new List<int>();
+        raceName.text = "";
+        raceCanFly.isOn = false;
+        raceCanHeal.isOn = false;
+        ReloadWeaponRace();
+    }
+
+    private bool AddRace(int newId = -1)
+    {
+        if (string.IsNullOrEmpty(raceName.text))
+        {
+            return false;
+        }
+        try
+        {
+            string name = raceName.text.Trim();
+            bool canFly = raceCanFly.isOn;
+            bool canHeal = raceCanHeal.isOn;
+
+            int id = newId == -1 ? (races.Count > 0 ? races.Select(x => x.id).Max() + 1 : 0) : newId;
+            if (races.Where(x => x.name == name).Count() > 0)
+            {
+                Debug.LogError("Weapons exist");
+                return false;
+            }
+
+            CharacterType newRace = new CharacterType(id, name, raceWeapons, canFly, canHeal);
+            races.Add(newRace);
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError(ex.Message);
+            return false;
+        }
+        return true;
+    }
+
+    private void LoadRace(string id)
+    {
+        CharacterType temp = races.Where(x => x.id.ToString() == id).First();
+        if (temp != null)
+        {
+            raceId = temp.id;
+            raceWeapons = temp.equipWeapon;
+            raceName.text = temp.name;
+            raceCanFly.isOn = temp.canFly;
+            raceCanHeal.isOn = temp.canHeal;
+            ReloadWeaponRace();
+        }
+    }
+
+    private void RemoveRace()
+    {
+        for (int i = 0; i < races.Count; i++)
+        {
+            if (races[i].id == raceId)
+            {
+                races.RemoveAt(i);
+                break;
+            }
+        }
+        raceId = -1;
+    }
+
+    private void UpdateRace()
+    {
+        int tempId = raceId;
+        CharacterType tempRace = new CharacterType();
+        for (int i = 0; i < races.Count; i++)
+        {
+            if (races[i].id == tempId)
+            {
+                tempRace = races[i];
+                races.RemoveAt(i);
+                break;
+            }
+        }
+        if (!AddRace(tempId))
+        {
+            races.Add(tempRace);
+        }
+    }
+
+    private void LoadRaceObjects()
+    {
+        ClearObjectList();
+        races.Sort((x, y) => { return x.id.CompareTo(y.id); });
+
+        for (int i = 0; i < races.Count; i++)
+        {
+            GameObject newObject = (GameObject)Instantiate(objectSelectPrefab, Vector3.zero, Quaternion.Euler(new Vector3()));
+            newObject.GetComponent<Button>().name = races[i].id.ToString();
+            newObject.GetComponent<Button>().GetComponentInChildren<Text>().text = races[i].name;
+            newObject.transform.SetParent(objectList);
+        }
+    }
+
+
+    public void AddWeaponRaceList()
+    {
+        int id = dicWeapon.Where(x => x.Value == raceWeapon.options[raceWeapon.value].text).FirstOrDefault().Key;
+        if (raceWeapons.Count == 0 || (!raceWeapons.Contains(id)))
+        {
+            raceWeapons.Add(id);
+        }
+
+        ReloadWeaponRace();
+    }
+
+    public void RemoveWeaponRaceList(string id)
+    {
+        raceWeapons.Remove(Convert.ToInt32(id));
+
+        ReloadWeaponRace();
+    }
+
+    private void ReloadWeaponRace()
+    {
+        ClearWeaponRaceList();
+        Dictionary<int, string> temp = dicWeapon.Where(x => raceWeapons.Contains(x.Key)).ToDictionary(y => y.Key, y => y.Value);
+
+        temp.OrderBy(x => x.Key);
+        foreach (var t in temp)
+        {
+            GameObject newObject = (GameObject)Instantiate(raceWeaponSelectPrefab, Vector3.zero, Quaternion.Euler(new Vector3()));
+            newObject.GetComponentInChildren<Button>().name = t.Key.ToString();
+            newObject.transform.Find("WeaponName").GetComponent<Text>().text = t.Value;
+            newObject.transform.SetParent(raceWeaponList);
+        }
+    }
+
+    private void ClearWeaponRaceList()
+    {
+        for (int i = 0; i < raceWeaponList.transform.childCount; i++)
+        {
+            Destroy(raceWeaponList.transform.GetChild(i).gameObject);
+        }
+    }
+
+    #endregion
 
     #region IO
     public void SaveObjects()
@@ -488,6 +692,7 @@ public class ObjectCreatorManager : MonoBehaviour
             ClearObjectList();
             createType = CreateType.none;
 
+            #region Item
             for (int i = 0; i < container.items.Count; i++)
             {
                 string name = container.items[i].name;
@@ -508,7 +713,42 @@ public class ObjectCreatorManager : MonoBehaviour
                 Item newItem = new Item(id, (ItemType)itemType, name, hp, atk, def, wis, dex, addHp, gold, price, useCharType, newCharType, sell);
                 items.Add(newItem);
             }
+            #endregion
 
+            #region Weapon
+            for (int i = 0; i < container.weapons.Count; i++)
+            {
+                string name = container.weapons[i].name;
+                int directAtk = container.weapons[i].directAtk;
+                int indirectAtk = container.weapons[i].indirectAtk;
+                int directWis = container.weapons[i].directWis;
+                int indirectWis = container.weapons[i].indirectWis;
+                int price = container.weapons[i].price;
+                bool sell = container.weapons[i].sell;
+                bool atkTwice = container.weapons[i].atkTwice;
+                int id = container.weapons[i].id;
+
+                Weapon newWeapon = new Weapon(id, name, directAtk, indirectAtk, directWis, indirectWis, price, sell, atkTwice);
+                weapons.Add(newWeapon);
+            }
+            #endregion
+
+            #region Race
+            for (int i = 0; i < container.charTyps.Count; i++)
+            {
+                string name = container.charTyps[i].name;
+                List<int> equipWeapons = container.charTyps[i].equipWeapons;
+                bool canFly = container.charTyps[i].canFly;
+                bool canHeal = container.charTyps[i].canHeal;
+                int id = container.charTyps[i].id;
+
+                CharacterType newRace = new CharacterType(id, name, equipWeapons, canFly, canHeal);
+                races.Add(newRace);
+            }
+            #endregion
+
+            SetWeapon();
+            SetRace();
 
         }
         catch (Exception ex)
@@ -529,12 +769,10 @@ public class ObjectCreatorManager : MonoBehaviour
     {
         DisableAllGroup();
         InitialObjects();
-        dicItemType = new Dictionary<int, string>();
-        dicRace = new Dictionary<int, string>();
-        dicWeapon = new Dictionary<int, string>();
 
         SetItemType();
         SetRace();
+        SetWeapon();
     }
 
     // Update is called once per frame
@@ -564,6 +802,8 @@ public class ObjectCreatorManager : MonoBehaviour
 
         itemUseRace.options.Clear();
         itemNewRace.options.Clear();
+        charRace.options.Clear();
+        dicRace = new Dictionary<int, string>();
 
         itemUseRace.options.Add(new Dropdown.OptionData() { text = "None" });
         itemNewRace.options.Add(new Dropdown.OptionData() { text = "None" });
@@ -578,6 +818,39 @@ public class ObjectCreatorManager : MonoBehaviour
         itemUseRace.RefreshShownValue();
         itemNewRace.RefreshShownValue();
         charRace.RefreshShownValue();
+
+        SetCharWeapon();
+    }
+
+    private void SetWeapon()
+    {
+        raceWeapon.options.Clear();
+        dicWeapon = new Dictionary<int, string>();
+
+        foreach (Weapon weapon in weapons)
+        {
+            dicWeapon.Add(weapon.id, weapon.name);
+            raceWeapon.options.Add(new Dropdown.OptionData() { text = weapon.name });
+        }
+        raceWeapon.RefreshShownValue();
+    }
+
+    public void SetCharWeapon()
+    {
+        charWeapon.options.Clear();
+        if (races.Count > 0)
+        {
+            CharacterType temp = races[dicRace.Where(x => x.Value == charRace.options[charRace.value].text).FirstOrDefault().Key];
+            List<int> tempList = temp.equipWeapon;
+
+            List<Weapon> tempWps = weapons.Where(x => tempList.Contains(x.id)).ToList();
+            foreach (Weapon weapon in tempWps)
+            {
+                charWeapon.options.Add(new Dropdown.OptionData() { text = weapon.name });
+            }
+        }
+
+        charWeapon.RefreshShownValue();
     }
 }
 
---
+++ Assets/Scripts/ObjectCreator/RemoveWeaponRaceList.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveWeaponRaceList : MonoBehaviour
+{
+    public void RemoveRaceWeapon()
+    {
+        ObjectCreatorManager.instance.RemoveWeaponRaceList(this.name);
+    }
+}
