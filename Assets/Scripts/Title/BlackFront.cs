3596934daebe50387640f1d6c4a52e1bbc754f2b
Solidsnake9058 <30305049+Solidsnake9058@users.noreply.github.com>
2017-11-02 21:37:11 +0800

    add title scene

---
+++ Assets/Scripts/Title/BlackFront.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackFront : MonoBehaviour
+{
+    public void Loading()
+    {
+        TitleManager.instance.ButtonStart();
+    }
+
+}
