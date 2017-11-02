5af47142f61642453ca71e337a0feb9e24c01f88
Solidsnake9058 <30305049+Solidsnake9058@users.noreply.github.com>
2017-11-03 00:03:48 +0800

    update

---
+++ Assets/Scripts/Title/TitleManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    public static TitleManager instance { get; private set; }
+
+    private void Start()
+    {
+        instance = this;
+    }
+
+    IEnumerator Loading()
+    {
+        AsyncOperation ao = SceneManager.LoadSceneAsync("GameScene");
+        ao.allowSceneActivation = false;
+        while (!ao.isDone)
+        {
+            if (ao.progress >= 0.9f)
+            {
+                ao.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+    }
+
+    public void ButtonStart()
+    {
+        StartCoroutine(Loading());
+    }
+
+    public void ButtonEndGame()
+    {
+        Application.Quit();
+    }
+}
